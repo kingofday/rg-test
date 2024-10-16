df7b83c84e264b6007c271c65e802c93075dfc24
Shahrooz Bazrafshan <kingofday.b@gmail.com>
2024-10-16 22:16:25 +0330

    feat: release domain

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename='https://kingofday.github.io/rg-test'>
         <App />
       </BrowserRouter>
   </React.StrictMode>
